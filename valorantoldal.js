0fa5ce5c5ad4e6cfa9b26bac14936aa23fb36fa3
KR_32 alias s1mpla <106810395+korki32@users.noreply.github.com>
2024-07-26 10:14:35 +0200

    Update valorantoldal.js

--- valorantoldal.js
+++ valorantoldal.js
@@ -1,6 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
     const agents = [
-        // Ügynökök adatai
+        { name: 'Sova', image: 'images/valorant/agent_sova.png' },
+        { name: 'Breach', image: 'images/valorant/agent_breach.png' },
+        { name: 'Skye', image: 'images/valorant/agent_skye.png' },
+        { name: 'KAY/O', image: 'images/valorant/agent_kayo.png' },
+        { name: 'Fade', image: 'images/valorant/agent_fade.png' },
+        { name: 'Gekko', image: 'images/valorant/agent_gekko.png' },
+        { name: 'Cypher', image: 'images/valorant/agent_cypher.png' },
+        { name: 'Sage', image: 'images/valorant/agent_sage.png' },
+        { name: 'Killjoy', image: 'images/valorant/agent_killjoy.png' },
+        { name: 'Chamber', image: 'images/valorant/agent_chamber.png' },
+        { name: 'Deadlock', image: 'images/valorant/agent_deadlock.png' },
+        { name: 'Jett', image: 'images/valorant/agent_jett.png' },
+        { name: 'Phoenix', image: 'images/valorant/agent_phoenix.png' },
+        { name: 'Reyna', image: 'images/valorant/agent_reyna.png' },
+        { name: 'Raze', image: 'images/valorant/agent_raze.png' },
+        { name: 'Yoru', image: 'images/valorant/agent_yoru.png' },
+        { name: 'Neon', image: 'images/valorant/agent_neon.png' },
+        { name: 'Iso', image: 'images/valorant/agent_iso.png' },
+        { name: 'Brimstone', image: 'images/valorant/agent_brimstone.png' },
+        { name: 'Viper', image: 'images/valorant/agent_viper.png' },
+        { name: 'Omen', image: 'images/valorant/agent_omen.png' },
+        { name: 'Astra', image: 'images/valorant/agent_astra.png' },
+        { name: 'Harbor', image: 'images/valorant/agent_harbor.png' },
+        { name: 'Clove', image: 'images/valorant/agent_clove.png' }
     ];
 
     const canvas = document.getElementById('wheel');
