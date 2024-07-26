31953bedc027edc8038c8a2d7d766bbade838e6c
KR_32 alias s1mpla <106810395+korki32@users.noreply.github.com>
2024-07-26 10:33:45 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
-document.getElementById('animatedButton').addEventListener('click', function() {
-    alert('Köszönöm, hogy rákattintottál!');
+document.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.nav-tab');
+    const slider = document.querySelector('.nav-tab-slider');
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', function () {
+            slider.style.width = this.offsetWidth + 'px';
+            slider.style.left = this.offsetLeft + 'px';
+        });
+    });
 });
